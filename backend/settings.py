f0c5e174f20829ee86b562b72ab430c9a07212e5
Frederik Voigt <frederik_v@hotmail.de>
2024-02-20 17:41:34 +0100

    Changed background color and buttons

--- backend/settings.py
+++ backend/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "backend.gui",
-    "tempus_dominus",
+    "tempus_dominus"
 ]
 TEMPUS_DOMINUS_DATE_FORMAT = "YYYY-MM-DD"
 
@@ -124,15 +124,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, "backend", "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "backend", "static/")
 STATIC_URL = "static/"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "backend", "gui", "static"),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
 
 FILE_UPLOAD_HANDLERS = [
     "django.core.files.uploadhandler.TemporaryFileUploadHandler",
--- todo.md
+++ todo.md
@@ -1,3 +1,17 @@
 # User Concepts
 
 - Visualize user concepts
+
+# Styling
+
+- User Menu as offcanvas sidebar
+- Form colors as in blue (blue slightly different opcacity)
+
+- change button size 
+- change button position at search and upload
+- input fields round
+- footer with link to contributor page
+
+# Security
+
+- Secret key as environment variable
