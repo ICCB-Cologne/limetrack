bf85b82714027982cbea69b7856a0f22422bd9ed
JG-IBSM <jonas.gassenschmidt@uniklinik-freiburg.de>
2024-06-04 13:30:10 +0200

    refining github workflow

--- backend/gui/selenium/create_test_users.py
+++ backend/gui/selenium/create_test_users.py
@@ -1,11 +1,6 @@
 from django.contrib.auth.models import User, Group
 
-user = User.objects.create_user('root', password='root')
-user.is_superuser = True
-user.is_staff = True
+user = User.objects.create_user('test_SPL', password='test4life')
 user.save()
-
-user2 = User.objects.create_user('test_SPL', password='test4life')
-user2.save()
-my_group = Group.objects.get(name='SPL')
-my_group.user_set.add(user2)
+new_group, created = Group.objects.get_or_create(name='SPL')
+new_group.user_set.add(user)
