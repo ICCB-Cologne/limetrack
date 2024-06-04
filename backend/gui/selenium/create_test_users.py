d655d95874d4b302d8789b2af52cf43175137445
JG-IBSM <jonas.gassenschmidt@uniklinik-freiburg.de>
2024-06-04 14:57:12 +0200

    refining github workflow

--- backend/gui/selenium/create_test_users.py
+++
@@ -1,6 +0,0 @@
-from django.contrib.auth.models import User, Group
-
-user = User.objects.create_user('test_SPL', password='test4life')
-user.save()
-new_group, created = Group.objects.get_or_create(name='SPL')
-new_group.user_set.add(user)
