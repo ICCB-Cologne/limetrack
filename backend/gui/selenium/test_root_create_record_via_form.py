c609260755b9cc09590c2f5217217b7dc68fcb9b
JG-IBSM <jonas.gassenschmidt@uniklinik-freiburg.de>
2024-06-04 09:31:30 +0200

    new try to get pytest to run as intended

--- backend/gui/selenium/test_root_create_record_via_form.py
+++ backend/gui/selenium/test_root_create_record_via_form.py
@@ -12,7 +12,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 class TestRootCreateRecordViaForm(BasicTestClass):
 
     def create_test_record(self):
-        self.driver.save_screenshot('screenie.png')
+        
         wait = WebDriverWait(self.driver, 5)
 
         wait.until(EC.element_to_be_clickable((By.ID, "id_recruiting_site")))
@@ -96,6 +96,7 @@ class TestRootCreateRecordViaForm(BasicTestClass):
             By.CSS_SELECTOR, ".modal-footer > .btn-primary").click()
 
     def delete_test_record(self):
+        self.driver.save_screenshot('screenie.png')
         self.driver.find_element(By.ID, "all-samples-nav").click()
         self.driver.find_element(By.ID, "Delete S3P-TEST1-5-L9-P-L1").click()
 
