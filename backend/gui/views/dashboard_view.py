a46ef90d1305c53fe9ef219989a2c3c99d20967b
JG-IBSM <jonas.gassenschmidt@uniklinik-freiburg.de>
2024-07-12 07:49:31 +0200

    draft dashboard with sample counting

--- backend/gui/views/dashboard_view.py
+++ backend/gui/views/dashboard_view.py
@@ -8,48 +8,65 @@ from typing import Any
 import plotly.graph_objects as go
 
 
+def count_samples_by_category(samples: list[HistopathologicalSample]):
+    plot_dicts = []
+
+    count_by = ["recruiting_site", "saturn3_sample_code"]
+
+    for c in count_by:
+        key_value_pairs = [
+            [getattr(instance, field.name)
+                for field in instance._meta.fields
+                if field.name == c]
+            for instance in samples
+        ]
+        keys = [site[0] for site in key_value_pairs]
+        keys = list(set(keys))
+        counter_dict = dict()
+
+        for key in keys:
+            if c == "saturn3_sample_code":
+                # count entities (S3C, S3P, S3M)
+                key = key[:3]
+            counter_dict.update({key: 0})
+
+        for key_value in key_value_pairs:
+            if c == "saturn3_sample_code":
+                counter_dict[key_value[0][:3]] += 1
+            else:
+                counter_dict[key_value[0]] += 1
+        plot_dicts.append(counter_dict)
+
+
 class DashboardView(LoginRequiredMixin, TemplateView):
     def get(self, request: HttpRequest,
             *args: Any, **kwargs: Any) -> HttpResponse:
         template_name = "gui/dashboard.html"
         samples = HistopathologicalSample.objects.all()
 
-        get_dict = request.GET
-        print(get_dict)
+        # get_dict = request.GET
 
-        if get_dict.get("count_what"):
-            count_this = get_dict["count_what"]
-        else:
-            count_this = "recruiting_site"
+        # if get_dict.get("count_what"):
+        #     count_this = get_dict["count_what"]
+        # else:
+        #     count_this = "recruiting_site"
 
-        real_sites = [
-            [getattr(instance, field.name)
-                for field in instance._meta.fields
-                if field.name == count_this]
-            for instance in samples
-        ]
-        keys = [site[0] for site in real_sites]
-        keys = list(set(keys))
-        d = dict()
-        for key in keys:
-            d.update({key: 0})
-        for site in real_sites:
-            d[site[0]] += 1
+        plot_dict = count_samples_by_category(samples)
 
-        fig1 = go.Figure(data=[go.Bar(x=list(d.keys()), y=list(d.values()),
-                                      marker_color="rgba(25,42,98,255)")])
+        figure_dict = {}
+        index = 1
+        for dic in plot_dict:
 
-        fig2 = go.Figure(data=[go.Bar(x=list(d.keys()), y=list(d.values()),
-                                      marker_color="rgba(150,77,0,255)")])
+            fig = go.Figure(
+                data=[go.Bar(x=list(dic.keys()), y=list(dic.values()),
+                             marker_color="rgba(25,42,98,255)")])
 
-        plot1 = fig1.to_html(full_html=False)
-        plot2 = fig2.to_html(full_html=False)
+            plot = fig.to_html(full_html=False)
+            figure_dict.update({f"plot{index}": plot})
+            index += 1
 
         context = {
-            "samples": real_sites,
-            "plot1": plot1,
-            "plot2": plot2,
             "user": request.user  # user, not username because we
-                                  # need to check the user's attributes
-        }
+                                  # need to check the user's attributes                                  
+            } | figure_dict
         return render(request, template_name, context=context)
