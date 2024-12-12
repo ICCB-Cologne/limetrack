7b3653021d02aff04e5a10309f9608e9b0a4202a
JG-IBSM <jonas.gassenschmidt@uniklinik-freiburg.de>
2024-12-12 07:37:49 +0100

    screenie

--- backend/gui/selenium/test_all_user_groups_entry_via_form.py
+++ backend/gui/selenium/test_all_user_groups_entry_via_form.py
@@ -36,6 +36,7 @@ class TestAllUserGroupsForm(BasicTestClass):
     def tum_input(self):
         self.login("test_TUM", "test4life")
         self.search_record()
+        self.driver.save_screenshot("screenie.png")
         self.driver.find_element(By.ID, "id_tissue_quality").send_keys(
             RecordGenerator.random_integer_from_1_to_5())
         self.driver.find_element(By.ID, "id_tumor_cell_content").send_keys(
