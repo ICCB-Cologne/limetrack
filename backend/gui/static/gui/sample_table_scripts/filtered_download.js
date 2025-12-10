4c691d089fd6c2be4ac58a8a4a59ea06ff0a0205
JG-IBSM <jonas.gassenschmidt@uniklinik-freiburg.de>
2025-12-10 14:20:51 +0100

    May this be the solution to large json downloads!

--- backend/gui/static/gui/sample_table_scripts/filtered_download.js
+++ backend/gui/static/gui/sample_table_scripts/filtered_download.js
@@ -55,7 +55,7 @@ function table_to_json(tablename) {
     json_buffer.push(record);
   }
 
-  return JSON.stringify(json_buffer);
+  return JSON.stringify({samples: json_buffer});
 }
 
 function download(filename, type, text) {
@@ -74,8 +74,80 @@ function download(filename, type, text) {
   document.body.removeChild(element);
 }
 
+function downloadNew(filename, type, blob) {
+  let mime_type = "text/csv;base64";
+
+  if (type == "Excel") {
+    mime_type = "application/vnd.ms-excel;base64";
+  }
+
+  let element = document.createElement("a");
+  element.setAttribute("href", `${window.URL.createObjectURL(blob)}`);
+  element.setAttribute("download", filename);
+  element.style.display = "none";
+  document.body.appendChild(element);
+  element.click();
+  document.body.removeChild(element);
+}
+
+
+
 function downloadWithFilter(type) {
-  let data = table_to_json("sampleTable");
+  let data = table_to_json("sampleTable")
+  // const bigAssJson = {
+  // userId: 123,
+  // items: Array.from({ length: 720000 }).map((_, i) => ({
+  //   id: i,
+  //   value: `Item ${i}`,
+  //   })),
+  // }; 
+  // let data = JSON.stringify(bigAssJson)
+
+  let fetch_type = "1"
+
+  switch (fetch_type){
+    case "1":
+      sendBigJson(data, type);
+      break;
+    case "2": 
+      sendBigJson2(data, type);
+      break;
+    case "3":
+      downloadWithFilterNew(type, data);
+      break;
+    case "4":
+      downloadWithFilterOld(type, data);
+      break;
+  }
+
+
+}
+
+function downloadWithFilterNew(type, data) {
+    console.log("NEW WAY")
+  let request = $.ajax({
+    type: "POST",
+    url: `/filtered_download/?type=${type}`,
+    beforeSend: (xhr) => {xhr.setRequestHeader("X-CSRFToken", document.getElementsByName("csrfmiddlewaretoken")[0].value);},
+    data: data,
+    dataType: "text",
+    success: (data, status, xhr) => {
+      console.log("SUCCESS is no LUCK!!!!");
+      let filename = xhr.getResponseHeader("filename");
+      console.log(filename);
+      console.log(type);
+      console.log(xhr.responseText);
+      
+      download(filename, type, xhr.responseText);
+    },
+    error: (e) => {
+      console.log("ERROR is LUCK!!!!");
+      console.log(e)},
+  });
+}
+
+function downloadWithFilterOld(type, data) {
+  // let data = table_to_json("sampleTable");
 
   let request = $.ajax({
     type: "POST",
@@ -88,8 +160,56 @@ function downloadWithFilter(type) {
     dataType: "text",
     success: (data, status, xhr) => {
       let filename = xhr.getResponseHeader("filename");
+      console.log(filename);
+      console.log(xhr.responseText)
       download(filename, type, xhr.responseText);
     },
     error: (e) => console.log(e),
   });
 }
+
+async function sendBigJson(data, type) {
+    console.log("2")
+    const bigPayload = data
+    try {
+      const response = await fetch(`/filtered_download/?type=${type}`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "X-CSRFToken": document.getElementsByName("csrfmiddlewaretoken")[0].value, // Django CSRF
+        },
+        body: bigPayload,
+      });
+      console.log("we here")
+
+      if (!response.ok) {
+        console.error("Server error:", response.status, await response.text());
+        return;
+      }
+      // const data = await response.json();
+      const filename = response.headers.get("filename")
+      const text = await response.text()
+      console.log(text)
+      console.log(filename);
+      // const blob = await response.blob()
+      // console.log(text)
+      console.log("Server replied:");
+      download(filename, type, text)
+
+    } catch (err) {
+      console.error("Network error:", err);
+    }
+  }
+
+
+
+  function sendBigJson2(data, type){
+    console.log("1")
+    var xmlhttp = new XMLHttpRequest();   // new HttpRequest instance 
+    var theUrl = `/filtered_download/?type=${type}`;
+    xmlhttp.open("POST", theUrl, true);
+    xmlhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xmlhttp.setRequestHeader("X-CSRFToken", document.getElementsByName("csrfmiddlewaretoken")[0].value); // Django CSRF);
+    xmlhttp.send(data);
+    downloadNew
+    }
